0dd331a60fcd8f4486897e5bcebd0147ea445f04
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2021-08-03 15:26:18 +1000

    modify login guidelines

--- src/components/GuideLine.js
+++ src/components/GuideLine.js
@@ -6,7 +6,8 @@ function GuideLine() {
         <div className="guide-line">
             <p> - user name requires at least <span style={{color: "red"}}>3</span> chars</p>
             <p> - password requires at least <span style={{color: "red"}}>5</span> chars</p>
-            <p className="guide-line-admin"> * If you want to log in as an admin, the password is the first five numbers of fibonacci nums</p>
+            <p className="guide-line-admin"> * If you want to log in as an admin@gmail.com, the password is the first five numbers of fibonacci nums</p>
+            <p className="guide-line-admin"> * Also implementing the password hashing and authentication by JSON web token</p>
         </div>
     )
 }
